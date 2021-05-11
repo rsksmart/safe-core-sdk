bf52ac99a5ab9b83d7ba700b9286b8e6335ca377
Antonio Morrone <antonio@iovlabs.org>
2021-05-11 17:40:11 +0200

    Apply prettier rules

--- src/EthersSafeFactory.ts
+++ src/EthersSafeFactory.ts
@@ -1,32 +1,49 @@
-import { Contract, ContractFactory, Signer, Event, ethers } from "ethers";
+import { Contract, ContractFactory, Signer, Event, ethers } from 'ethers'
 import GnosisSafeProxyFactory from '@gnosis.pm/safe-contracts/build/contracts/GnosisSafeProxyFactory.json'
-import GnosisSafe from '@gnosis.pm/safe-contracts/build/contracts/GnosisSafe.json';
-import Safe from "Safe";
-import { ZERO_ADDRESS } from "./utils/constants";
-import EthersSafe from "./EthersSafe";
+import GnosisSafe from '@gnosis.pm/safe-contracts/build/contracts/GnosisSafe.json'
+import Safe from 'Safe'
+import { ZERO_ADDRESS } from './utils/constants'
+import EthersSafe from './EthersSafe'
 
 class EthersSafeFactory {
-
-    static async createSafe(safeSingletonAddress: string, signer: Signer, owners: string[], threshold?: number): Promise<Safe> {
-        if ( threshold === undefined) {
-            threshold = owners.length
-        }
-        if (threshold <= 0) {
-            throw new Error('Invalid threshold: it must be greater than or equal to 0')
-        }
-        if ( threshold > owners.length ) {
-            throw new Error('Invalid threshold: it must be lower than or equal to owners length')
-        }
-        const proxyFactoryFactory = new ContractFactory(GnosisSafeProxyFactory.abi, GnosisSafeProxyFactory.bytecode, signer)
-        const proxyFactory = await proxyFactoryFactory.deploy()
-        const createProxyTx = await proxyFactory.createProxy(safeSingletonAddress, '0x')
-        const receipt = await createProxyTx.wait()
-        const proxyAddress = receipt.events.find((e: Event) => e.event === 'ProxyCreation').args[0]
-
-        const gnosisSafe = new Contract(proxyAddress, GnosisSafe.abi, signer)
-        await gnosisSafe.setup(owners, threshold, ZERO_ADDRESS, "0x", ZERO_ADDRESS, ZERO_ADDRESS, 0, ZERO_ADDRESS)
-        return await EthersSafe.create(ethers, gnosisSafe.address, signer)
+  static async createSafe(
+    safeSingletonAddress: string,
+    signer: Signer,
+    owners: string[],
+    threshold?: number
+  ): Promise<Safe> {
+    if (threshold === undefined) {
+      threshold = owners.length
     }
+    if (threshold <= 0) {
+      throw new Error('Invalid threshold: it must be greater than or equal to 0')
+    }
+    if (threshold > owners.length) {
+      throw new Error('Invalid threshold: it must be lower than or equal to owners length')
+    }
+    const proxyFactoryFactory = new ContractFactory(
+      GnosisSafeProxyFactory.abi,
+      GnosisSafeProxyFactory.bytecode,
+      signer
+    )
+    const proxyFactory = await proxyFactoryFactory.deploy()
+    const createProxyTx = await proxyFactory.createProxy(safeSingletonAddress, '0x')
+    const receipt = await createProxyTx.wait()
+    const proxyAddress = receipt.events.find((e: Event) => e.event === 'ProxyCreation').args[0]
+
+    const gnosisSafe = new Contract(proxyAddress, GnosisSafe.abi, signer)
+    await gnosisSafe.setup(
+      owners,
+      threshold,
+      ZERO_ADDRESS,
+      '0x',
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      0,
+      ZERO_ADDRESS
+    )
+    return await EthersSafe.create(ethers, gnosisSafe.address, signer)
+  }
 }
 
 export default EthersSafeFactory
--- tests/creation.test.ts
+++ tests/creation.test.ts
@@ -3,49 +3,49 @@ import chaiAsPromised from 'chai-as-promised'
 import { BigNumber } from 'ethers'
 import { deployments, ethers, waffle } from 'hardhat'
 import EthersSafe from '../src'
-import EthersSafeFactory from "../src/EthersSafeFactory";
+import EthersSafeFactory from '../src/EthersSafeFactory'
 
 import { getSafeWithOwners } from './utils/setup'
 chai.use(chaiAsPromised)
 
 describe('Safe creation', () => {
-    const [user1, user2, user3] = waffle.provider.getWallets()
+  const [user1, user2, user3] = waffle.provider.getWallets()
 
-    const setupTests = deployments.createFixture(async ({ deployments }) => {
-        await deployments.fixture()
-        const SafeDeployment = await deployments.get('GnosisSafe')
-        return {
-            safeSingletonAddress: SafeDeployment.address
-        }
+  const setupTests = deployments.createFixture(async ({ deployments }) => {
+    await deployments.fixture()
+    const SafeDeployment = await deployments.get('GnosisSafe')
+    return {
+      safeSingletonAddress: SafeDeployment.address
+    }
+  })
+
+  describe('EthersSafeFactory.create', () => {
+    const owners = [user1.address, user2.address, user3.address]
+
+    it('should fail if the threshold is less than or equal to zero', async () => {
+      const { safeSingletonAddress } = await setupTests()
+      await chai
+        .expect(EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners, -1))
+        .rejectedWith('Invalid threshold: it must be greater than or equal to')
     })
 
-    describe('EthersSafeFactory.create', () => {
-        const owners = [user1.address, user2.address, user3.address]
-        
-        it('should fail if the threshold is less than or equal to zero', async () => {
-            const { safeSingletonAddress } = await setupTests()
-            await chai
-                .expect(EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners, -1))
-                .rejectedWith('Invalid threshold: it must be greater than or equal to')
-        })
-        
-        it('should fail if the threshold is greater than the owners length', async () => {
-            const { safeSingletonAddress } = await setupTests()
-            await chai
-                .expect(EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners, 4))
-                .rejectedWith('Invalid threshold: it must be lower than or equal to owners length')
-        })
-
-        it('should successfully create a safeSDK instance if the threshold is set properly', async () => {
-            const { safeSingletonAddress } = await setupTests()
-            const safeSdk = await EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners, 2)
-            chai.expect(safeSdk).not.to.be.undefined
-        })
-
-        it('should successfully create a safeSDK instance if the threshold is not set', async () => {
-            const { safeSingletonAddress } = await setupTests()
-            const safeSdk = await EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners)
-            chai.expect(safeSdk).not.to.be.undefined
-        })
+    it('should fail if the threshold is greater than the owners length', async () => {
+      const { safeSingletonAddress } = await setupTests()
+      await chai
+        .expect(EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners, 4))
+        .rejectedWith('Invalid threshold: it must be lower than or equal to owners length')
     })
+
+    it('should successfully create a safeSDK instance if the threshold is set properly', async () => {
+      const { safeSingletonAddress } = await setupTests()
+      const safeSdk = await EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners, 2)
+      chai.expect(safeSdk).not.to.be.undefined
+    })
+
+    it('should successfully create a safeSDK instance if the threshold is not set', async () => {
+      const { safeSingletonAddress } = await setupTests()
+      const safeSdk = await EthersSafeFactory.createSafe(safeSingletonAddress, user1, owners)
+      chai.expect(safeSdk).not.to.be.undefined
+    })
+  })
 })
