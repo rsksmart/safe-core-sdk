1e0e4b43221366d9ab55beff96f824a7d6d7c464
Antonio Morrone <antonio@iovlabs.org>
2021-05-14 11:48:24 +0200

    Export EthersSafeFactory

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import EthersSafeFactory from './EthersSafeFactory'
 import { SafeSignature } from 'utils/signatures/SafeSignature'
 import EthersSafe from './EthersSafe'
 import Safe from './Safe'
 import SafeTransaction, { SafeTransactionDataPartial } from './utils/transactions/SafeTransaction'
 
 export default EthersSafe
-export { Safe, SafeSignature, SafeTransactionDataPartial, SafeTransaction }
+export { Safe, SafeSignature, SafeTransactionDataPartial, SafeTransaction, EthersSafeFactory }
